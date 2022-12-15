ee30810b723caf781c66343b6901142453fa9780
Mr.Schaffman <61944369+mrSchaffman@users.noreply.github.com>
2022-12-15 11:58:30 +0100

    Adding Resources

---
+++ Clock/Clock/resource.h
@@ -0,0 +1,30 @@
+#define IDD_APP2_DIALOG			102
+#define IDS_APP_TITLE			103
+#define IDD_ABOUTBOX			104
+#define IDM_ABOUT				105
+#define IDM_EXIT				106
+#define IDI_SMALL				108
+#define IDI_APPICON				109
+
+#define IDC_APP_MENU			110
+#define IDC_APP_ACC				111
+#define IDR_MAINFRAME			128
+#define IDS_APP_MENU			128
+
+
+#ifndef IDC_STATIC
+#define IDC_STATIC				-1
+#endif
+
+// Next default values for new objects
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+
+#define _APS_NO_MFC					130
+#define _APS_NEXT_RESOURCE_VALUE	129
+#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_CONTROL_VALUE		1000
+#define _APS_NEXT_SYMED_VALUE		110
+#endif
+#endif
